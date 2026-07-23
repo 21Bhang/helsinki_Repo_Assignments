041c8ac91bf1abf2699a0d4e562e1fb371367019
21Bhang <uhuribhang211@gmail.com>
2026-07-23 17:26:48 +0300

    Exercise 7.1: Create useField Hook Goal: Create a custom useField hook for form input management.

---
+++ OSA7/routed-anecdotes/step1/src/components/CreateNew.jsx
@@ -0,0 +1,49 @@
+import { useField } from "../hooks";
+
+const CreateNew = (props) => {
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    });
+  };
+
+  const handleReset = () => {
+    content.onChange({ target: { value: "" } });
+    author.onChange({ target: { value: "" } });
+    info.onChange({ target: { value: "" } });
+  };
+
+  return (
+    <div>
+      <h2>Create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateNew;
