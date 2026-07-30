0b13e232225aaa87f432d5b70a8363833e692a92
21Bhang <uhuribhang211@gmail.com>
2026-07-30 12:55:35 +0300

    Exercise 7.13: Added like and delete functionality to Zustand store

---
+++ OSA7/routed-anecdotes/Blog-App/client/src/stores/blogStore.js
@@ -0,0 +1,28 @@
+import { create } from 'zustand'
+import blogService from '../services/blogs'
+
+const useBlogStore = create(set => ({
+  blogs: [],
+  fetchBlogs: async () => {
+    const blogs = await blogService.getAll()
+    set({ blogs })
+  },
+  createBlog: async content => {
+    const newBlog = await blogService.create(content)
+    set(state => ({ blogs: [...state.blogs, newBlog] }))
+    return newBlog
+  },
+  likeBlog: async id => {
+    const blog = useBlogStore.getState().blogs.find(b => b.id === id)
+    const updatedBlog = await blogService.update(id, { ...blog, likes: blog.likes + 1 })
+    set(state => ({
+      blogs: state.blogs.map(b => (b.id !== id ? updatedBlog : b)),
+    }))
+  },
+  deleteBlog: async id => {
+    await blogService.remove(id)
+    set(state => ({ blogs: state.blogs.filter(b => b.id !== id) }))
+  },
+}))
+
+export default useBlogStore
